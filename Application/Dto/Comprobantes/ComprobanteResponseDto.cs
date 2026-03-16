5800bc100f563cd0fb7cde23a7f6dbf76b53a4d0
Rody Castro <therodycc@gmail.com>
2026-03-16 01:58:08 -0400

    Fix: Missing Id to Comprobante Dto Response

--- Application/Dto/Comprobantes/ComprobanteResponseDto.cs
+++ Application/Dto/Comprobantes/ComprobanteResponseDto.cs
@@ -1,5 +1,6 @@
 public class ComprobanteResponseDto
 {
+    public int Id { get; set; }
     public string NCF { get; set; }
     public decimal Monto { get; set; }
     public decimal Itbis18 { get; set; }
